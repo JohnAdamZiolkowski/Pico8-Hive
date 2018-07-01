49a8b62c4cb02097473f16eb82fb61b29dd6cdcd
JohnAdamZiolkowski <johnadamziolkowski@gmail.com>
2018-07-01 10:41:12 -0400

    add shift and caps lock ability to print

--- prince/delayer.lua
+++ prince/delayer.lua
@@ -8,18 +8,25 @@ function inttobin(b)
  return t
 end
 
-chars=" abcdefghijklmnopqrstuvwxyz0123456789"
-s2c={}
-c2s={}
-for i=1,#chars do
- local c=i-2
- local s=sub(chars,i,i)
- c2s[c]=s
- s2c[s]=c
+function get_chars(sheet)
+	assert(type(sheet)=="table")
+ sheet.s2c={}
+ sheet.c2s={}
+ for i=1,#sheet.chars do
+  local c=i-2
+  local s=sub(sheet.chars,i,i)
+  sheet.c2s[c]=s
+  sheet.s2c[s]=c
+ end
 end
 
-function ord(s,i)
- return s2c[sub(s,i or 1,i or 1)]
+function ord(sheet, s, i)
+ assert(type(sheet)=="table")
+ assert(type(s)=="string")
+ assert(type(i)=="number")
+ ci = sheet.s2c[sub(s,i or 1,i or 1)]
+ assert(ci, s..".."..i)
+ return ci
 end
 
 cls(13)
@@ -32,6 +39,7 @@ wide = {
  th = 5,
  layers = 4
 }
+get_chars(wide)
 
 slim = {
  chars = " abcdefghijklmnopqrstuvwxyz0123456789.,!?:'+-*/()",
@@ -41,6 +49,7 @@ slim = {
  th = 5,
  layers = 4
 }
+get_chars(slim)
 
 enemy = {
 	x = 0,
@@ -50,14 +59,24 @@ enemy = {
  layers = 2
 }
 
-function print(string, x, y, pc)
- local offset = 0
- for char=1,#string do
 
-  ci = ord(string, char)
-  sheet = slim
-  render(sheet, ci, x + offset, y, pc)
- 	offset += sheet.tw + 1
+function print(string, x, y, pc, caps)
+ local offset = 0
+ local shift = false
+ for char=1,#string do
+  if sub(string,char,char) == "^" then
+   shift = true
+  else
+   if shift or caps then
+    sheet = wide
+   else
+    sheet = slim
+   end
+   ci = ord(sheet, string, char)
+   render(sheet, ci, x + offset, y, pc)
+  	offset += sheet.tw + 1
+  	shift = false
+  end
  end
 end
 
@@ -91,15 +110,15 @@ function draw_enemy()
 
 end
 
-print("abcdefghijklm", 1, 10, 0)
-print("nopqrstuvwxyz", 1, 20, 1)
-print("0123456789   ", 1, 30, 2)
+print("abcdefghijklm", 1, 10, 0, true)
+print("nopqrstuvwxyz", 1, 20, 1, true)
+print("0123456789   ", 1, 30, 2, true)
 
 print("abcdefghijklm", 1, 40, 3)
 print("nopqrstuvwxyz", 1, 50, 4)
-print("0123456789   ", 1, 60, 5)
+print("0123456789.,!?:'+-*/()", 1, 60, 5)
 
-print("hello world", 1, 80, 6)
+print("hello world", 1, 80, 6, true)
 print("hello world", 1, 90, 7)
-
+print("^hello ^world!", 1, 100, 9)
 //draw_enemy(126, 100, 100)
