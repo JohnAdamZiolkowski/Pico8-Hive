51cc961fbd56fbcbcd085c1bfc0a5dad95379874
JohnAdamZiolkowski <johnadamziolkowski@gmail.com>
2018-07-01 10:06:14 -0400

    render each character separately

--- prince/delayer.lua
+++ prince/delayer.lua
@@ -25,6 +25,7 @@ end
 cls(13)
 
 wide = {
+ chars = " abcdefghijklmnopqrstuvwxyz0123456789",
  x = 0,
  y = 0,
  tw = 5,
@@ -33,6 +34,7 @@ wide = {
 }
 
 slim = {
+ chars = " abcdefghijklmnopqrstuvwxyz0123456789.,!?:'+-*/()",
 	x = 0,
 	y = 5,
  tw = 3,
@@ -48,58 +50,56 @@ enemy = {
  layers = 2
 }
 
-function print(ps, px, py, pc, draw_wide)
- if draw_wide then
-  render(ps, px, py, pc, wide)
- else
-  render(ps, px, py, pc, slim)
+function print(string, x, y)
+ local offset = 0
+ for char=1,#string do
+
+  ci = ord(string, char)
+  sheet = slim
+  render(sheet, ci, x + offset, y)
+ 	offset += sheet.tw + 1
  end
 end
 
-function render(ps, px, py, pc, sheet)
+function render(sheet, ci, dx, dy)
 
- for char=1,#ps do
-  ci = ord(ps,char)
+ tw = sheet.tw
+ th = sheet.th
+ off_x = sheet.x
+ off_y = sheet.y
+ layers = sheet.layers
 
-  tw = sheet.tw
-  th = sheet.th
-  off_x = sheet.x
-  off_y = sheet.y
-  layers = sheet.layers
+ local c = flr(ci / layers)
+ local l = ci % layers
+ local sx = off_x + c * tw
+ local sy = off_y
 
-  local c = flr(ci / layers)
-  local l = ci % layers
-  local sx = off_x + c * tw
-  local sy = off_y
-  local dx = px + (char-1) * (tw+1)
-  local dy = py
-
-  for y = 0,th-1 do
-   for x = 0,tw-1 do
-    local spixel = sget(sx + x, sy + y)
-    local b = inttobin(spixel)
-    if b[l+1] == 1 then
-     spixel = pc
-     pset(dx + x, dy + y, spixel)
-    end
+ for y = 0,th-1 do
+  for x = 0,tw-1 do
+   local spixel = sget(sx + x, sy + y)
+   local b = inttobin(spixel)
+   if b[l+1] == 1 then
+    spixel = pc
+    pset(dx + x, dy + y, spixel)
    end
   end
  end
+
 end
 
 function draw_enemy()
 
 end
 
-print("abcdefghijklm", 1, 10, 0, true)
-print("nopqrstuvwxyz", 1, 20, 0, true)
-print("0123456789   ", 1, 30, 0, true)
+print("abcdefghijklm", 1, 10)
+print("nopqrstuvwxyz", 1, 20)
+print("0123456789   ", 1, 30)
 
-print("abcdefghijklm", 1, 40, 0, false)
-print("nopqrstuvwxyz", 1, 50, 0, false)
-print("0123456789   ", 1, 60, 0, false)
+print("abcdefghijklm", 1, 40)
+print("nopqrstuvwxyz", 1, 50)
+print("0123456789   ", 1, 60)
 
-print("hello world", 1, 80, 0, true)
-print("hello world", 1, 90, 0, false)
+print("hello world", 1, 80)
+print("hello world", 1, 90)
 
 //draw_enemy(126, 100, 100)
